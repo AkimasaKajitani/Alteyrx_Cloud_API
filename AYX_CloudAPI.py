43b2da8cad887c3e2d71f3ced68a18d87f4f2db5
Akimasa Kajitani <76108949+AkimasaKajitani@users.noreply.github.com>
2025-12-07 21:46:56 +0900

    Add files via upload

---
+++ AYX_CloudAPI.py
@@ -0,0 +1,144 @@
+import urllib.parse
+import urllib.request
+import json
+
+# ============= 使い方 =============================================
+# 外部JSONファイルにCloudで取得したOAuth2.0 APIトークンのアクセストークン、リフレッシュトークンを貼り付けて保存
+USER_AGENT = "AYX_CloudAPI by AK"
+
+# ============= 関数 =============================================
+# JSONファイルの読み込み
+def _read_tokens(json_file):
+    try:
+        with open(json_file, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            access_token = data.get('access_token')
+            refresh_token = data.get('refresh_token')
+            return access_token, refresh_token
+    except FileNotFoundError:
+        print(f"{json_file}が見つかりません")
+        return None, None
+    except json.JSONDecodeError:
+        print("JSONの読み込みに失敗しました")
+        return None, None
+
+# JSONファイルへのトークンの書き込み
+def _write_tokens(json_file, access_token, refresh_token):
+    data = {
+        'access_token': access_token,
+        'refresh_token': refresh_token
+    }
+    with open(json_file, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+    print("トークンを保存しました")
+
+# トークンの更新
+def _refresh_tokens(refresh_token, refresh_url, oauth2_client_id):
+  headers = {
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
+  body = {
+    'grant_type': 'refresh_token',
+    'refresh_token': refresh_token,
+    'client_id': oauth2_client_id
+  }
+ 
+  # URL encode the body for the refresh request
+  encoded_body = urllib.parse.urlencode(body).encode()
+
+  # Make the refresh request
+  request = urllib.request.Request(refresh_url, data=encoded_body, method='POST')
+  with urllib.request.urlopen(request) as response:
+    return json.load(response)
+
+# ===== 外部から呼び出す用関数 =====
+# トークン更新
+def update_tokens(refresh_url, oauth2_client_id, json_file):
+    # JSONファイルから読み込み
+    access_token_json, refresh_token_json = _read_tokens(json_file)
+    # トークンを更新
+    new_tokens = _refresh_tokens(refresh_token_json, refresh_url, oauth2_client_id)
+    # 更新されたトークンを保存
+    _write_tokens(json_file, new_tokens['access_token'], new_tokens['refresh_token'])
+
+    return new_tokens['access_token']
+
+# Cloud設定読み込み
+def load_cloud_setting(json_file):
+    # JSONファイルから読み込み
+    try:
+        with open(json_file, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            ayx_cloud_url = data.get('ayx_cloud_url')
+            refresh_url = data.get('refresh_url')
+            oauth_client_id = data.get('oauth_client_id')
+
+            print("クラウド設定を読み込みました")
+            print(f"ayx_cloud_url : {ayx_cloud_url}\nrefresh_url : {refresh_url}\noauth_client_id : {oauth_client_id}")
+            return ayx_cloud_url, refresh_url, oauth_client_id
+    except FileNotFoundError:
+        print(f"{json_file}が見つかりません")
+        return None, None, None
+    except json.JSONDecodeError:
+        print("JSONの読み込みに失敗しました")
+        return None, None, None
+
+# 現在のワークスペース情報を取得
+def get_current_workspace(aac_url, access_token):
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'User-Agent': USER_AGENT,
+    }
+    print(headers)
+    request = urllib.request.Request(f'{aac_url}/iam/v1/workspaces/current', headers=headers)
+    with urllib.request.urlopen(request) as response:
+        return json.load(response)
+
+# 請求情報取得
+def get_current_billing_accounts(aac_url, access_token):
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'User-Agent': USER_AGENT,
+    }
+    print(headers)
+    request = urllib.request.Request(f'{aac_url}/billing/v1/my/billing-accounts/current', headers=headers)
+    with urllib.request.urlopen(request) as response:
+        return json.load(response)
+
+
+# ============= For Debug =============================================
+if __name__ == "__main__":
+
+    # 固定値
+    JSON_FILE_PATH = "credential.json"
+    CLOUD_SETTING_JSON = "cloud_setting.json"
+
+    # クラウド設定の読み込み
+    AYX_CLOUD_URL, REFRESH_URL, OAUTH_CLIENT_ID = load_cloud_setting(CLOUD_SETTING_JSON)
+
+    # アクセストークン更新
+    access_token_json, refresh_token_json = _read_tokens(JSON_FILE_PATH)
+
+    new_tokens = _refresh_tokens(refresh_token_json,REFRESH_URL,OAUTH_CLIENT_ID)
+    # 更新されたトークンを保存
+    _write_tokens(JSON_FILE_PATH,new_tokens['access_token'],new_tokens['refresh_token'])
+
+    print("New Access Token:", new_tokens['access_token'])
+    print("New Refresh Token:", new_tokens['refresh_token'])
+
+    # ワークスペースID取得
+    workspaceinfo = get_current_workspace(AYX_CLOUD_URL, new_tokens['access_token'])
+
+    print(workspaceinfo)
+    print(workspaceinfo['id'])
+
+    # 請求情報取得
+    billinginfo = get_current_billing_accounts(AYX_CLOUD_URL, new_tokens['access_token'])
+
+    print(billinginfo)
+    print(billinginfo['data']['workspaces']) # ワークスペースID取得
+
+
+
+else:
+        pass
